2b6c26da241c75d12f1d366a2d6761a8d25b00f4
Aleksandr <alx@nsbill.ru>
2017-11-02 13:10:36 +0300

    ver0.004b

--- check_usermpd.py
+++ check_usermpd.py
@@ -10,77 +10,87 @@ HOST1 = "10.77.200.6"
 USER1 = 'admin'
 PASSWORD1 = 'PASSWD'
 PORT1 = '6010'
+TIMEOUT = 5
 
 # NAS2
 HOST2 = "10.77.200.6"
 USER2 = 'admin'
 PASSWORD2 = 'PASSWD'
 PORT2 = '6005'
+TIMEOUT = 5
 
-#def usage():
-#    print ("""Usage: check_usermpd [-h|--help] [-login|--login]"
-#    User Online""")
-#    sys.exit(3)
-#print(sys.argv)
+def nas(host,port,user,password,timeout):
+    ''' Подключение к NAS серверу по telnet к FreeBSD MPD5 '''
+    try:
+        tn = telnetlib.Telnet(host,port,timeout)
+        tn.read_until(b"Username: ")
+        tn.write(user.encode('ascii') + b"\n")
+        if password:
+            tn.read_until(b"Password: ")
+            tn.write(password.encode('ascii') + b"\n")
 
-def getUsers():
-    ''' Online user '''
-    sys.exit(3)
-#print(sys.argv[1])
-
-def nas(host,port,user,password):
-    tn = telnetlib.Telnet(host,port)
-    tn.read_until(b"Username: ")
-    tn.write(user.encode('ascii') + b"\n")
-    if password:
-        tn.read_until(b"Password: ")
-        tn.write(password.encode('ascii') + b"\n")
-
-    tn.write(b"show sessions\n")
-    tn.write(b"exit\n")
-    data = (tn.read_all().decode('ascii'))
-    return data
+        tn.write(b"show sessions\n")
+        tn.write(b"exit\n")
+        data = (tn.read_all().decode('ascii'))
+        return data
+    except Exception:
+        data = None
+        return data
 
 try:
-    login = sys.argv[1]
+    login = sys.argv[1]  # проверяем агрумент с nagios
 except Exception:
     Status = 3
     print('UNKNOW - Status=%s of user' % Status)
+    print('__login__')
     sys.exit(3)
 
 def check_user(ns_data,login=None):
-    ii=ns_data.split('\r\n')
-    info = []
-    for v in ii:
-        v = v.split('\t')
-        if len(v) == 9:
-            v = (dict(zip(('ng','ip',3,4,'vlan',6,7,'user','mac'),v)))
-            info.append(v)
-    up = []
-    for value in info:
+    ''' Проверяем пользователя на серверах и парсим результат '''
+    if ns_data:
+        ii=ns_data.split('\r\n')
+        info = []
+        for v in ii:
+            v = v.split('\t')
+            if len(v) == 9:
+                v = (dict(zip(('ng','ip',3,4,'vlan',6,7,'user','mac'),v)))
+                info.append(v)
+        up = []
+        for value in info:
 #        print('===LOGIN = %s ===' % login)
-        if value.get('user') == login:
+            if value.get('user') == login:
 #            Status = 0
-            up.append((value.get('ng'), value.get('ip'), value.get('mac'), value.get('vlan')))
+                up.append((value.get('ng'), value.get('ip'), value.get('mac'), value.get('vlan')))
 #            print('Connection is UP - %s of user' % login)
 #            return sys.exit(0)
 #    print(up)
 #    print('Connection is DOWN - %s of user' % login)
 #    return sys.exit(1)
-    return up
-try:
-    ns1_data=nas(HOST1,PORT1,USER1,PASSWORD1)
-    ns2_data=nas(HOST2,PORT2,USER2,PASSWORD2)
+        return up
+    else:
+        up = []
+        return up
 
-    st1 = check_user(ns1_data,login)
-#    print(st1)
-    st2 = check_user(ns2_data,login)
-#    print(st2)
+try:
+    ns1_data=nas(HOST1,PORT1,USER1,PASSWORD1,TIMEOUT) # подключаемся к NAS1 
+#    print(ns1_data)
+    ns2_data=nas(HOST2,PORT2,USER2,PASSWORD2,TIMEOUT) # подключаемся к NAS2  
+#    print(ns2_data)
+    if ns1_data == None and ns2_data == None:
+        print('ERROR: server are unavailable')
+        sys.exit(1)
+    else:
+        st1 = check_user(ns1_data,login) # выборка результата с NAS1
+#        print(st1)
+        st2 = check_user(ns2_data,login) # выборка результата с NAS2 
+#        print(st2)
+
     st = st1 + st2
     if len(st) > 1:
         print(st)
         sys.exit(2)
     elif len(st) == 0:
+        print('User is not connected')
         sys.exit(1)
     else:
         print(st)
